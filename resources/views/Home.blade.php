7839b2d66bb13b1da031da179ffae31ee9ce171d
Franpan94 <francescopanico94@libero.it>
2022-09-06 16:47:10 +0200

    style shift

--- resources/views/Home.blade.php
+++ resources/views/Home.blade.php
@@ -1,24 +1,25 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
+    <style>
+        .width {
+            width: 500px;
+        }
+
+        .text-center {
+            text-align: center;
+        }
+    </style>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Home</title>
 </head>
-<body  class="text-center">
+
+<body class="text-center">
     <h1>Hello Laravel</h1>
     <img src="https://res.cloudinary.com/practicaldev/image/fetch/s--X2WDI7b6--/c_imagga_scale,f_auto,fl_progressive,h_900,q_auto,w_1600/https://dev-to-uploads.s3.amazonaws.com/i/2jlduu7hh4ry0q77akap.png" class="width" alt="">
 </body>
+
 </html>
-
-
-<style>
-    .width{
-        width: 500px;
-    }
-
-    .text-center{
-        text-align: center;
-    }
-</style>
